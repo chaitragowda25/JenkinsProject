3e05bbc791096f7b4d48349000259b641b6c56b9
admin <admin@CHAITRA>
2023-06-28 19:40:51 +0100

    Jenkins

---
+++ src/test/java/scripts/Test3.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 
+{
+	@Test
+	public void tc3()
+	{
+		Reporter.log("Hello Sql",true);
+	}
+}
+
